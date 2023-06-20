7c9e0ee0ba69fcc5fa6d84f14ad8c90099f8dbed
lamiyahell <axira1108@gmail.com>
2023-06-20 18:21:20 +0300

    1 commit

---
+++ src/test/java/sky/pro/EmployeeBook/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.EmployeeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
