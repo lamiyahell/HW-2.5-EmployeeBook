462532c0deb84cc5f653695658549da623c753bd
lamiyahell <axira1108@gmail.com>
2023-07-02 21:10:36 +0300

    ДЗ 2.7 сделано

--- src/main/java/sky/pro/EmployeeBook/service/EmployeeServiceImpl.java
+++ src/main/java/sky/pro/EmployeeBook/service/EmployeeServiceImpl.java
@@ -7,27 +7,25 @@ import sky.pro.EmployeeBook.exception.EmployeeNotFoundException;
 import sky.pro.EmployeeBook.exception.EmployeeStorageIsFullException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final List<Employee> employeeList = new ArrayList<>(List.of(
-            new Employee("Petr", "Petrov"),
-            new Employee("Anna", "Morozova"),
-            new Employee("Max","Maximov"),
-            new Employee("Jon", "Lenon")
-    ));
+    private final Map<String, Employee> employees;
     int maxCounts = 5;
 
-//    public EmployeeServiceImpl(List<Employee> employeeList) {
-//        this.employeeList = employeeList;
-//    }
+    public EmployeeServiceImpl() {
+        this.employees = new HashMap<>();
+    }
 
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
-        } else if (!employeeList.contains(employee) && employeeList.size() < maxCounts) {
-            employeeList.add(employee);
+        } else if (!employees.containsKey(employee.getFullName()) && employees.size() < maxCounts) {
+            employees.put(employee.getFullName(), employee);
             return employee;
         }
         throw new EmployeeStorageIsFullException();
@@ -35,22 +33,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            employeeList.remove(employee);
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+            return employees.remove(employee.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
 
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employeeList.contains(employee)) {
-            return employee;
+        if (employees.containsKey(employee.getFullName())) {
+            return employees.get(employee.getFullName());
         }
         throw new EmployeeNotFoundException();
     }
 
     public List<Employee> findAll() {
-        return new ArrayList<>(employeeList);
+        return new ArrayList<>(employees.values());
     }
 }
